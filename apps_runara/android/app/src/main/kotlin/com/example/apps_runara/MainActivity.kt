62396f0ab4e30a09975f763fa1f7670f2ac03471
inzaghi <tauramohamadinzaghi@apps.ipb.ac.id>
2025-09-06 10:48:05 +0700

    run

--- apps_runara/android/app/src/main/kotlin/com/example/apps_runara/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.apps_runara
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- apps_runara/lib/ui/app_colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kBgBlue   = Color(0xFF0D1B3D);
-const kCardBlue = Color(0xFF152449);
-const kAccent   = Color(0xFF9AA6FF);
-const kSubtle   = Color(0xFFBFC3D9);
